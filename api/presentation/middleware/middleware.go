6eb713a03f522cdbbf2ce264822af81059c5fbad
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2021-01-19 20:43:45 +0900

    middlewareの改変

--- api/presentation/middleware/middleware.go
+++ api/presentation/middleware/middleware.go
@@ -15,6 +15,16 @@ import (
 func CommonMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		// CORS
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
